09070acc6fde379f628d690c642f83ad28edb689
subexa <subexa93@gmail.com>
2023-01-13 18:29:26 +1100

    Add login page tests, login data and login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,3 +3,7 @@ import "@testing-library/cypress/add-commands"
 before(() => {
   cy.configureCypressTestingLibrary({ testIdAttribute: "data-test" })
 })
+
+Cypress.Commands.add("Login", (username, password) => {
+  // cy.request()
+})
