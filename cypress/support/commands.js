122c907818c10d54fc689b0919217fbf68919bb9
subexa <subexa93@gmail.com>
2023-01-13 18:53:44 +1100

    Add custom login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,16 @@
 import "@testing-library/cypress/add-commands"
+import loginData from "../fixtures/login-data.json"
+const standardUser = loginData.usernames[0]
+const password = loginData.password
 
 before(() => {
   cy.configureCypressTestingLibrary({ testIdAttribute: "data-test" })
 })
 
-Cypress.Commands.add("Login", (username, password) => {
-  // cy.request()
+Cypress.Commands.add("login", (username, password) => {
+  // Better approach is to make an API call using cy.request()
+  cy.visit("/")
+  cy.findByTestId("username").type(username)
+  cy.findByTestId("password").type(password)
+  cy.findByRole("button", { name: "Login" }).click()
 })
