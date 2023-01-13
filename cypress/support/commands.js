9994fadf34ce55ad31d8624b659968a19b41db96
subexa <subexa93@gmail.com>
2023-01-13 23:31:09 +1100

    Add sortingByName and sortingByPrice commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,4 @@
 import "@testing-library/cypress/add-commands"
-import loginData from "../fixtures/login-data.json"
-const standardUser = loginData.usernames[0]
-const password = loginData.password
 
 before(() => {
   cy.configureCypressTestingLibrary({ testIdAttribute: "data-test" })
@@ -13,4 +10,47 @@ Cypress.Commands.add("login", (username, password) => {
   cy.findByTestId("username").type(username)
   cy.findByTestId("password").type(password)
   cy.findByRole("button", { name: "Login" }).click()
+})
+
+Cypress.Commands.add("checkSortingByName", (className, sortingValue) => {
+  cy.get(className).then((items) => {
+    let listOfProducts = []
+    for (let i = 0; i < items.length; i++) {
+      listOfProducts.push(items[i].innerHTML)
+    }
+
+    let sortedListsOfProducts
+    if (sortingValue === "asc") {
+      sortedListsOfProducts = listOfProducts.slice().sort()
+    } else {
+      sortedListsOfProducts = listOfProducts.slice().sort().reverse()
+    }
+    expect(listOfProducts).to.have.ordered.members(sortedListsOfProducts)
+  })
+})
+
+Cypress.Commands.add("checkSortingByPrice", (className, sortingValue) => {
+  cy.get(className).then((items) => {
+    let listOfProducts = []
+    let priceSortedList = []
+    for (let i = 0; i < items.length; i++) {
+      let priceString = items[i].innerHTML.slice(1)
+      listOfProducts.push(Number(priceString))
+    }
+
+    if (sortingValue === "asc") {
+      priceSortedList = listOfProducts.slice().sort(function (a, b) {
+        return a - b
+      })
+    } else {
+      priceSortedList = listOfProducts
+        .slice()
+        .sort(function (a, b) {
+          return a - b
+        })
+        .reverse()
+    }
+
+    expect(listOfProducts).to.have.ordered.members(priceSortedList)
+  })
 })
