0e91def7f9bec6092b586055dcc25c137c0c1778
subexa <subexa93@gmail.com>
2023-01-13 17:49:59 +1100

    Configure cypress to work with testing library

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
 import "@testing-library/cypress/add-commands"
 
-cy.configureCypressTestingLibrary({ testIdAttribute: "data-test" })
+before(() => {
+  cy.configureCypressTestingLibrary({ testIdAttribute: "data-test" })
+})
