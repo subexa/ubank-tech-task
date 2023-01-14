ca375ca5079715ae413a8af2255ad1f4033a8f5a
subexa <subexa93@gmail.com>
2023-01-14 11:08:11 +1100

    Add openBurgerMenu command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -64,3 +64,13 @@ Cypress.Commands.add("checkSortingByPrice", (className, sortingValue) => {
     expect(listOfProducts).to.have.ordered.members(priceSortedList)
   })
 })
+
+Cypress.Commands.add("openBurgerMenu", () => {
+  cy.findByRole("button", { name: "Open Menu" }).click()
+})
+
+Cypress.Commands.add("resetAppState", () => {
+  cy.openBurgerMenu()
+  cy.findByRole("link", { name: "Reset App State" }).click()
+  cy.get(".shopping_cart_badge").should("not.exist")
+})
